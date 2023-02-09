d7bf6fdc4f190820d001dc5e6a639ad64a46e34c
Max <skyrider2003@gmail.com>
2023-02-08 21:32:45 -0800

    made the portfolio JS and flexbox work.

---
+++ project.php
@@ -0,0 +1,26 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Max's Portfolio</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="portfolio">
+    <nav>
+        <a href="/index.html" class="nav-max">Max K</a>
+        <div class="nav-links">
+            <a href="/portfolio.html">Portfolio</a>
+            <a href="/about.html">About Me</a>
+            <a href="/contact.html">Contact Me</a>
+        </div>
+    </nav>
+        
+</body>
+</html>
